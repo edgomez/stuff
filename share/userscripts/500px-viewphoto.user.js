96123c9857bea399002ef9141fea8ccdc76cd8c7
Edouard Gomez <ed.gomez@free.fr>
2018-11-27 15:59:36 +0100

    500px userscript: improve it a bit

--- share/userscripts/500px-viewphoto.user.js
+++ share/userscripts/500px-viewphoto.user.js
@@ -1,25 +1,35 @@
 // ==UserScript==
 // @name 500px view link
-// @version 20181124.01
+// @version 20181127.01
 // @author GomGom
 // @description  This script tries to add a view link to view 500px photo in a new tab.
 // @include http*://web.500px.com/*
+// @include http*://500px.com/*
 // @grant GM_addStyle
 // ==/UserScript==
 
 (
 function ()
 {
-    GM_addStyle("img.photo-show__img { z-index:999 !important; }");
-
-    var addBtnDownload = function (src)
+    var addViewLink = function (src)
     {
-        var intab = document.querySelector('.viewintab');
-        if (intab == null) {
-            var el = document.querySelector('.topnav__icon--upload');
-            el.insertAdjacentHTML('afterEnd', '<a href="' + src + '" target="_blank" download class="viewintab button medium submit">View</a>');
+        var viewlink = document.getElementById('viewlink_uscript');
+        if (viewlink == null) {
+            viewlink = document.createElement('a');
+            viewlink.innerHTML = 'Photo Link';
+            viewlink.style.cssText = 'font-weight: bold; color: white;';
+            viewlink.download = true;
+            viewlink.href = src;
+            viewlink.id = 'viewlink_uscript';
+
+            var container = document.createElement('div');
+            container.style.cssText = 'position: absolute; right: 1ex; top: 10em; z-index: 9999 !important; background: #34bf49; border-radius: 5px; padding: 5px;';
+            container.appendChild(viewlink);
+            document.body.appendChild(container);
+
+            console.log('500px viewlink: attached link to DOM');
         } else {
-            intab.setAttribute('href', src);
+            viewlink.setAttribute('href', src);
         }
     }
 
@@ -29,8 +39,8 @@ function ()
             function (event) {
                 if (event.type == "attributes" && event.attributeName == 'src') {
                     if (event.target.src.indexOf('h%3D300/') == -1 && event.target.alt != "" && event.target.className == "photo-show__img") {
-                        addBtnDownload(event.target.src);
-                        // console.log(event.target, event.attrChange, event.attrName, event.newValue);
+                        console.log('500px viewlink: found matching event');
+                        addViewLink(event.target.src);
                     }
                 }
             }
@@ -53,3 +63,4 @@ function ()
 }
 )
 ();
+
